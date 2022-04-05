25ba1f7816650b9be4f9af53f403618ce2a83b00
ccosta568 <costachristopher90@gmail.com>
2022-04-04 23:34:00 -0400

    first commit

---
+++ src/main/java/calculatorproject/Main.java
@@ -0,0 +1,10 @@
+package calculatorproject;
+
+public class Main {
+		public static void main(String[] args) {
+		    Calculator c=new Calculator();
+		    
+		    System.out.println("Adding Two Numbers : " +c.add(2, 2));
+		}
+		}
+
